e40a78a1cba271cd67afaaa21d6e3f45e7adae40
K7ITECH <k7itech@gmail.com>
2024-06-14 12:06:13 +0530

    This is my person class

---
+++ Person.java
@@ -0,0 +1,16 @@
+  class Person{
+    Person(){    
+       System.out.println("I am inside person class constructor");
+    }
+  }
+  
+  class PersonManager{
+  
+     public static void main(String args[]){
+       
+           Person p1 =  new Person();
+           Person p2 =  new Person();
+           Person p3 =  new Person();
+           
+     }
+  }
