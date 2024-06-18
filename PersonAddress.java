7edd5e9bd870229c8f21c981499aa8c90750522d
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-06-18 10:55:13 +0530

    added PersonAddress class

---
+++ PersonAddress.java
@@ -0,0 +1,11 @@
+class Person{
+public static void main(String args[]){
+System.out.println("I am person");
+}
+}		
+class Address{
+public static void main(String args[]){
+System.out.println("I am address of person");
+}
+}
+		
