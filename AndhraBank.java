2455bbf88aa0278d258522be09d45bca2ec2cd0e
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-06-18 11:16:52 +0530

    added AndhraBank class

---
+++ AndhraBank.java
@@ -0,0 +1,42 @@
+class StateBank{
+int accountNumber;
+String accountHolder;
+String address;
+static int noOfBranches;
+StateBank(){
+System.out.println("s1.address");
+System.out.println("s2.accountNumber");
+}
+public static void main(String args[]){
+StateBank s1=new StateBank();
+StateBank s2=new StateBank();
+s1.accountNumber=32140764;
+s1.address="renigunta";
+s1.noOfBranches=2;
+s2.accountNumber=23456126;
+s2.noOfBranches=4;
+System.out.println("noOfBranches:"+s1.noOfBranches);
+System.out.println("noOfBranches:"+s2.noOfBranches);
+}
+}
+public class AndhraBank{
+int accountNumber;
+String accountHolder;
+String address;
+static int noOfBranches;
+AndhraBank(){
+System.out.println("a1.address");
+System.out.println("a2.accountNumber");
+}
+public static void main(String args[]){
+StateBank a1=new andhraBank();
+StateBank a2=new andhraBank();
+a1.accountNumber=32140764;
+a1.address="renigunta";
+a1.noOfBranches=2;
+a2.accountNumber=23456126;
+a2.noOfBranches=4;
+System.out.println("noOfBranches:"+a1.noOfBranches);
+System.out.println("noOfBranches:"+a2.noOfBranches);
+}
+}
