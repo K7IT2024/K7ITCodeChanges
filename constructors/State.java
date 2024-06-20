fb56570c4dcf4f133af6188bfc8dfa7fc01da843
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-06-20 14:01:48 +0530

    added Constructors

---
+++ constructors/State.java
@@ -0,0 +1,29 @@
+class State{
+static int chiefMinisters;
+static int noOfDistricts;
+String name;
+Double population;
+State(){
+System.out.println("s1.name);
+System.out.println("s2.name");
+}
+public static void main(String args[]){
+State s1=new State();
+s1.chiefMinisters=1;
+s1.noOfDistricts=26;
+s1.name="andhraPradesh";
+s1.population=6.7;
+State s2=new State();
+s2.chiefMinisters=4;
+s2.noOfDistricts=30;
+s2.name="andhraPradesh";
+s2.population=6.8;
+System.out.println("chiefMinisters:"+s1.chiefMinisters);
+System.out.println("noOfDistricts:"+s1.noOfDistricts);
+System.out.println("name:"+s2.name);
+System.out.println("population:"+s1.population);
+System.out.println("population:"+s2.population);
+}
+}
+
+
