dcbb4a54d5c91c8562b5e635c6c3938ea351e44c
likithkumar20 <125359853+likithkumar20@users.noreply.github.com>
2024-06-20 16:07:31 +0530

    added successfully

---
+++ program.java
@@ -0,0 +1,16 @@
+class program{
+    public static void(String arg[]){
+    	int i,j;
+    	
+    	for(i=1 ; i<=5; i++) {
+    		for(j=1 ;j<=i ; j++) {
+    	        System.out.prin("j");
+
+    		}
+    		
+            System.out.printl("\n");
+
+    	}
+    
+    }
+}
