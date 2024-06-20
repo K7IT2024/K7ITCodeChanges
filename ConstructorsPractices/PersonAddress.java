bcf84f8216095d901a06ac09977e2148b2915d53
K7ITECH <k7itech@gmail.com>
2024-06-20 11:12:08 +0530

    constructorPractices

---
+++ ConstructorsPractices/PersonAddress.java
@@ -0,0 +1,18 @@
+              class Person{
+		 public static void main(String args[]){
+		   System.out.println("I am person");
+		 }
+
+		}
+
+		class Address{
+		  static public void main(String[] args){
+		   System.out.println("I am address of person");
+		 }
+		}
+
+		class Car{
+		  
+                }
+
+		
