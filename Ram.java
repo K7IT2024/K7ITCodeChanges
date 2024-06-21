fbb3c3370800d120486516e7a5de78e52fe31144
Tripurala Tharun <tripuralatharun>
2024-06-21 12:34:51 +0530

    Ram class added

---
+++ Ram.java
@@ -0,0 +1,22 @@
+class Ram{
+int age;
+String name;
+String colour;
+double height;
+double weight;
+public static void main(String args[]){
+	 Ram r1 = new Ram();
+	r1.age = 27;
+	r1.name = "bunny";
+	r1.colour = "white";
+	r1.height = 5.8;
+	r1.weight = 65.1;
+System.out.println("Ram details");
+System.out.println("age:"+r1.age);
+System.out.println("name:"+r1.name);
+System.out.println("colour:"+r1.colour);
+System.out.println("height:"+r1.height);
+System.out.println("weight:"+r1.weight);
+}
+}
+
