618a64646cd7000678b224e5fa89cf572550ee0c
Lucky Vinnu <mgnanasekhar01@gmail.com>
2024-06-21 19:12:17 +0530

    over

---
+++ constructors.java
@@ -0,0 +1,12 @@
+//constructors
+ class constructors{
+
+	int x;
+	public constructors(int y){
+		x=y;
+	}
+	public static void main(String [] args){
+		 constructors myCon=new constructors(5);
+		System.out.println(myCon.x);
+	}
+}
