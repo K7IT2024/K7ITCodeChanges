7d800e898c0da256807ec881b436ba38182a7af5
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-06-22 11:04:49 +0530

    hbvdv

---
+++ Adress.java
@@ -0,0 +1,6 @@
+class Address{
+		 public static void main(String args[]){
+		   System.out.println("I am address of person");
+		 }
+		}
+		
---
+++ Car.java
@@ -0,0 +1,5 @@
+class Car{
+		 public static void main(String args[]){
+		   System.out.println("I am car of person");
+		 }
+		}
