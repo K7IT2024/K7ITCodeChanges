c104be335ebf33f78c267a72d9225d8341dfbb82
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-07-10 16:15:25 +0530

    added fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,47 @@
+console.log("hi");
+console.log("what the hell is life");
+M=4
+N=3
+O=M+N
+console.log(O)
+var x=5
+var x=6
+var y=7
+var z=x+y
+console.log(z)
+var g=1
+g=2
+var h=7
+var i=g+h
+console.log(i)
+let a=10
+let b=7
+let c=a+b
+console.log(c)
+let u=3
+ u=2
+let v=2
+let w=u+v
+console.log(w)
+let d=1
+ d=2
+let e=3
+let f=d+e
+console.log(f)
+const r=5
+const s=3
+const t=r+s
+console.log(t)
+var vegetable="potato"
+console.log(vegetable)
+var fruit="apple"
+var fruit="banana"
+console.log(fruit)
+let animal="lion"
+animal="tiger"
+console.log(animal)
+const bird="peacock"
+console.log(bird)
+
+
+
