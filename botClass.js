444a8fab99c534d2277947bc987e705ea470c8f9
gnana9347 <mgnanasekhar01@gmail.com>
2024-07-10 16:07:35 +0530

    classes n functions

---
+++ botClass.js
@@ -0,0 +1,18 @@
+class botClass{
+    
+    constructor(name,age,weight,height){
+        this.name=name;
+        this.age=age;
+        this.weight=weight;
+        this.height=height;
+    }
+   
+}
+b1=new botClass("lucky",12,30,3);
+b2=new botClass("vinnu",10,20,2);
+console.log(b1);
+console.log(b1.name);
+console.log(b1.weight);
+console.log(b1.age);
+console.log(b2.name);
+
---
+++ functions.js
@@ -0,0 +1,20 @@
+function lucky(a,b){
+     c=a+b;
+     console.log(c);
+     evenOdd();
+
+}
+lucky(2,3);
+
+function evenOdd()
+{
+    let f=5;
+    let g=6;
+    if(f%2==0 || g%2==0){
+        console.log("true");
+    }else{
+        console.log("false");
+    }
+
+}
+
