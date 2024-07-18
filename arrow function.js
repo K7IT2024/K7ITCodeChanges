91f243fa985be444ef58965cf710426ba5078479
arthalaprasad <125430674+arthalaprasad@users.noreply.github.com>
2024-07-18 14:49:19 +0530

    add arrow functions and conditional statements

---
+++ arrow function.js
@@ -0,0 +1,26 @@
+let myfunction=()=>"HelloWorld"
+console.log(myfunction())
+let sum=(a,b)=> a+b;
+console.log(sum(8,10));
+let mult=(c,d)=>{
+    console.log("multiplication of values")
+    return c*d
+}
+console.log(mult(9,9))
+let div=(c,d)=>{
+    return c/d
+}
+console.log(div(4,2))
+let multi=(e,f,g)=>{
+    return e*f*g
+}
+e=2
+f=7
+g=3
+console.log(multi(e,f,g))
+
+
+
+
+
+
---
+++ condition statements.js
@@ -0,0 +1,59 @@
+let number=0
+if(number==4){
+    console.log("valid number")
+}
+else{
+    console.log("not valid number")
+}
+let String="mouni"
+if(String=="prasad"){
+    console.log("wife of prasad")
+}
+else{
+    console.log("spouse of prasad")
+}
+let string="abc"
+if(string=="abcd"){
+    console.log("valid number");
+}else if(string=="abc"){
+    console.log("partially Valid");
+}else{
+    console.log("not valid");
+}
+var temperature=25
+if(temperature>30){
+    console.log("its a hot day")
+}
+else if((temperature<20)&&(temperature>30)){
+    console.log("its a pleasant day")
+}
+else{
+    console.log("its a cold day")
+}
+const z=""
+if(z>0){
+    if(z==6){
+        console.log("valid number")
+    }
+    else{
+        console.log("not a valid number")
+    }
+}else{
+    console.log("please enter a correct number")
+}
+let fruit=""
+switch(fruit){
+case("apple"):
+console.log(("fruit is red"))
+break
+case("banana"):
+console.log("fruit is yellow")
+break
+case("orange"):
+console.log("fruit is orange")
+break
+default:
+console.log("i dont know this fruit name")
+}
+
+      
