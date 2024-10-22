2da8176c009ce67463f1e54c93db80156f6dfad0
gnana9347 <mgnanasekhar01@gmail.com>
2024-10-22 12:11:08 +0530

    deleted

--- A.java
+++
@@ -1,9 +0,0 @@
-package com.k7it;
-
-public class A {
-
-	public static void main(String[] args) {
-		System.out.println("hi");
-	}
-
-}
--- Protect.java
+++
@@ -1,6 +0,0 @@
-package com.k7it.pack1;
-
-public class Protect {
-	protected int i=10;
-	
-}
