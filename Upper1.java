36c6ec332ecf9f519d44ae9c33b17f3ed84fbe55
gnana9347 <mgnanasekhar01@gmail.com>
2024-11-14 13:40:58 +0530

    over

---
+++ Upper1.java
@@ -0,0 +1,28 @@
+package StringPack1;
+
+public class Upper1 {
+
+	public  String 
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Upper1 u = new Upper1();
+		
+		String s3 = "hello this is hello tharun";
+		
+		String[] s4 = s3.split("");
+		
+		String result = u.firstLetterUpper(s4);
+		
+	}
+
+	private String firstLetterUpper(String[] s4) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ UpperCase1.java
@@ -0,0 +1,21 @@
+package StringPack1;
+
+public class UpperCase1 {
+	
+
+private static String UpperCase1() {
+	
+	
+	
+	
+	
+}
+	
+	
+		
+		
+		
+		
+		
+
+}
