23cdb87b18c4895d2f9a661e9d72da7c429cbbc7
dairymilksai <dairymilksai2@gmail.com>
2024-11-14 14:47:50 +0530

    s

---
+++ roman.js
@@ -0,0 +1,79 @@
+/*let a= "123a";
+let b = Number("123");
+let c= Number(a);
+console.log(typeof(a));
+console.log(b);
+console.log(typeof(c));
+console.log(c);
+console.log (a)
+ 
+
+
+
+
+
+
+let  x=123;
+let y=String(x);
+console.log(typeof(y));
+console.log (y)
+console.log (x)*/
+
+
+
+
+
+
+
+
+let d = new Date();
+console.log(typeof(d));
+console.log(d);
+console.log(Number(d));
+sai= d.getTime();
+console.log(sai);
+
+
+
+
+let strDate= String(d);
+console.log((strDate))
+console.log(typeof(strDate));
+console.log(d.toString());
+console.log((strDate));
+
+let bn=true;
+console.log(Number(bn));
+let sb=false;
+console.log(Number(sb));
+
+
+
+
+let num=543;
+console.log(Boolean(num));
+
+
+
+console.log(typeof(String(bn)));
+ console.log(true.toString());
+
+
+
+
+
+
+
+
+let laptop={"company name":"lenova",color:"black",size:16, weight:2};
+let computer={color:"black",size:16, weight:2};
+const person={name:"Tharun",color:"white",height:6, weight:60}
+const person1={"person name":"Babu",color:"white",height:6.5, weight:65, age:100};
+console.log(laptop);
+console.log(laptop["company name"]);
+console.log(laptop.color);
+console.log(person1["person name"]);
+console.log(person.name);
+console.log(laptop["company name"]);
+console.log(person1["age"]);
+
