48498457c49ebc58f82eede366dc6279f493e2a7
gnana9347 <mgnanasekhar01@gmail.com>
2024-11-14 11:12:29 +0530

    adding the files

---
+++ Animal.java
@@ -0,0 +1,43 @@
+package com.Tharun.pack2;
+
+public interface Animal {
+	
+    int age = 10;
+    String name= "dog";
+    
+	void putLegs();
+	void putTail();
+	void putMask();
+
+}
+
+
+class Dog implements Animal{
+	
+	public void PutLegs() {
+		
+		System.out.println("put legs in dogs ");
+	}
+	
+	
+	public void putTail() {
+		
+		System.out.println("put tail for dog");
+	}
+	
+	
+      public void putMask() {
+		
+		System.out.println("put tail for dog");
+	}
+
+
+	@Override
+	public void putLegs() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
